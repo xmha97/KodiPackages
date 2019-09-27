680d8a45ca4535ee5f042dab8d66a97fa20257d1
Amir Ammari <amirrammari@outlook.com>
2019-09-27 10:35:15 +0330

    Compatible with Python 3

--- plugin.audio.navahang/main.py
+++ plugin.audio.navahang/main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+
+#  Copyright 2019 Amir Ammari
+#
+#  This file is part of the Navahang kodi plugin.
+#
+#  This plugin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This plugin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this plugin.  If not, see <http://www.gnu.org/licenses/>.
 
 #
 # Imports
